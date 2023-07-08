5c67b618711513abb1c73c0ccd036ffb7e722497
ndegz <ndegwa8781@gmail.com>
2023-07-08 12:23:53 +0300

    landing page completed

--- src/App.js
+++ src/App.js
@@ -9,6 +9,9 @@ import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
 import NavDropdown from 'react-bootstrap/NavDropdown';
 import Image  from 'react-bootstrap/Image';
+import { Link } from 'react-scroll';
+
+
 function App() {
   return (
     <div>
@@ -19,14 +22,17 @@ function App() {
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
           <Nav className="me-auto">
-            <Nav.Link href="#home">Home</Nav.Link>
-            <Nav.Link href="#link">Link</Nav.Link>
             <NavDropdown title="Sections" id="basic-nav-dropdown">
-              <NavDropdown.Item href="#action/3.1"></NavDropdown.Item>
-              <NavDropdown.Item href="#action/3.2">
+              <NavDropdown.Item>
+                <Link to="ft" smooth={true} duration={500}>
                 Features
+                </Link>
               </NavDropdown.Item>
-              <NavDropdown.Item href="#action/3.3">About</NavDropdown.Item>
+              <NavDropdown.Item>
+              <Link to="abt" smooth={true} duration={500}>
+                About
+                </Link>
+                </NavDropdown.Item>
             </NavDropdown>
           </Nav>
         </Navbar.Collapse>
@@ -37,18 +43,17 @@ function App() {
       <h2 className='intro'>Welcome :) </h2>
       <p className='phrase'>BreakBoost is an application that serves as a pomodoro timer 
         helping you to complete daily tasks in a productive and efficient way!</p>
-      <Button className="sitebutton" href="">Site</Button>
+      <Button className="sitebutton" href="https://breakboost.vercel.app" style={{"backgroundColor": "black", "border":"2px solid black"}}>Site</Button>
     </div>
 
     <main>
-      <h2 className='features'>Features</h2>
+      <h2 className='features' id="ft">Features</h2>
       <div className='gallery'>
       <Container>
         <Row className='row'>
           <Col sm={4}>
-          d
           <Image className="image" style={{"width":"100%", "height":"80" }} src="https://i.postimg.cc/sDnLrg1S/taskbar.png" />
-         
+            <div>
             <h4>Taskbar</h4>
           <p>A task bar where you set up your daily tasks</p></div>
           </Col>
@@ -56,7 +61,7 @@ function App() {
           <div className='set-toggle'>
           <Image style={{"width":"100%", "height":"80" }} src='https://i.postimg.cc/3rsL4cj2/settings.png' />
         
-            <h4>Timer Toggle</h4>
+            <h4>Slider Toggle</h4>
             <p>Slider toggle to set up your timer in minutes to set break and work time</p>
             </div>
           </Col>
@@ -69,6 +74,15 @@ function App() {
           </Col>
         </Row>
       </Container>
+      </div>
+
+      <div className='about' id='abt'>
+        <h2 className='about-title'>About</h2>
+        <p className='inspire'>The inspiration for this project came about due to the rigorous tasks that one is expected to accomplish at ALX.
+          It is key that time management is observed during the program and tasks are finished on time and you are able to track which tasks are finished and pending, I saw fit to create a personal tool that could help observe that as well as emphasize the importance of taking regular breaks while working on projects.
+        </p>
+        <p>Link to repository: <a href="https://github.com/ndegwa007/Breakboost">github</a></p>
+
       </div>
     </main>
     </div>
